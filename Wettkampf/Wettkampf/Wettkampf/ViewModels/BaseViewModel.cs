df8ae58d1bd35553b351d19d05538f96a53afcd7
Christian Müller <christian.mueller@hfu.ch>
2022-06-15 15:08:39 +0200

    Fix formatting in BaseViewModel.cs

--- Wettkampf/Wettkampf/Wettkampf/ViewModels/BaseViewModel.cs
+++ Wettkampf/Wettkampf/Wettkampf/ViewModels/BaseViewModel.cs
@@ -10,23 +10,25 @@ namespace Wettkampf.ViewModels
 {
     public class BaseViewModel : INotifyPropertyChanged
     {
-        public IDataStore<Item> DataStore => DependencyService.Get<IDataStore<Item>>();
-
-        bool isBusy = false;
         public bool IsBusy
         {
-            get { return isBusy; }
-            set { SetProperty(ref isBusy, value); }
+            get => _isBusy;
+            set => SetProperty(ref _isBusy, value);
         }
 
-        string title = string.Empty;
         public string Title
         {
-            get { return title; }
-            set { SetProperty(ref title, value); }
+            get => _title;
+            set => SetProperty(ref _title, value);
         }
 
-        protected bool SetProperty<T>(ref T backingStore, T value,
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected IDataStore<Item> DataStore => DependencyService.Get<IDataStore<Item>>();
+
+        protected bool SetProperty<T>(
+            ref T backingStore,
+            T value,
             [CallerMemberName] string propertyName = "",
             Action onChanged = null)
         {
@@ -39,8 +41,6 @@ namespace Wettkampf.ViewModels
             return true;
         }
 
-        #region INotifyPropertyChanged
-        public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
         {
             var changed = PropertyChanged;
@@ -49,6 +49,8 @@ namespace Wettkampf.ViewModels
 
             changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
-        #endregion
+
+        private bool _isBusy;
+        private string _title = string.Empty;
     }
 }
