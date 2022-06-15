c3ca6f5b0f9381bde09709269baa0a9618c7dc99
Christian Müller <christian.mueller@hfu.ch>
2022-06-15 17:45:17 +0200

    Klasse Mitglied hinzugefügt

---
+++ Wettkampf/Wettkampf/Wettkampf/Models/Mitglied.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wettkampf.Models
+{
+    internal class Mitglied
+    {
+        public string Id { get; set; }
+        public string Nachname { get; set; }
+        public string Vorname { get; set; }
+    }
+}
