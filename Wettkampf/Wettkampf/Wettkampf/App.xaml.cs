e69a90a530c3430041f5126ac819772f4ef4f670
Christian Müller <christian.mueller@hfu.ch>
2022-06-15 14:50:09 +0200

    Neues Projekt erstellt

---
+++ Wettkampf/Wettkampf/Wettkampf/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Wettkampf.Services;
+using Wettkampf.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Wettkampf
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Wettkampf/Wettkampf/Wettkampf/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Wettkampf.ViewModels;
+using Xamarin.Forms;
+
+namespace Wettkampf.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
