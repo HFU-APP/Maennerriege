71da6ac79a750c28e58bf1d8aae1319485fddc1b
Christian Müller <christian.mueller@hfu.ch>
2022-07-06 16:41:45 +0200

    Tests hinzugefügt
    -> Wenn Tests einzel ausgeführt werden, funktionieren sie.
    -> Wenn alle zusammen ausgeführt werden, schlagen sie aufgrund folgendem Fehler fehl:
    System.InvalidOperationException : The container can't be changed after the first call to GetInstance, GetAllInstances, Verify, and some calls of GetRegistration. Please see https://simpleinjector.org/locked to understand why the container is locked. The following stack trace describes the location where the container was locked:

--- Wettkampf/Wettkampf.Tests/CoreTests.cs
+++ Wettkampf/Wettkampf.Tests/CoreTests.cs
@@ -1,26 +1,18 @@
-﻿// <copyright file="CoreTests.cs" company="Marco von Ballmoos">
-//   Copyright (c) 2021 Marco von Ballmoos. All rights reserved.
-// </copyright>
-
-using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using FakeItEasy;
 using Wettkampf.Core;
-using Wettkampf.Views;
 using NUnit.Framework;
-using SimpleInjector;
 using Turnverein.ViewModels;
 using Wettkampf.Models;
 using Wettkampf.Services;
 using Wettkampf.ViewModels;
-using Xamarin.Forms;
 
 namespace Wettkampf.Tests
 {
   [TestFixture]
   public class CoreTests
   {
-    [Test]
+      [Test]
     public async Task TestSaveEmptyPersonList()
     {
       var container = ContainerExtensions
@@ -39,63 +31,93 @@ namespace Wettkampf.Tests
       A.CallTo(() => dialogService.Show(A<string>.Ignored, A<string>.Ignored)).MustHaveHappenedOnceExactly();
     }
 
-        //[Test]
-        //public async Task TestInserPerson()
-        //{
+        [Test]
+        public async Task TestAddPerson()
+        {
+            var dialogService = A.Fake<IDialogService>();
+            App.Services.RegisterInstance(dialogService);
 
+            var factory = A.Fake<IItemFactory<Verein>>();
+            App.Services.RegisterInstance(factory);
 
-        //    Verein verein = new Verein();
+            Verein verein = new Verein();
 
-        //    ListViewModelBase<Verein, VereineViewModel> a = new ListViewModelBase<Verein, VereineViewModel>();
-        //    int old = a.Items.Count;
+            ListViewModelBase<Verein, VereineViewModel> a = new ListViewModelBase<Verein, VereineViewModel>();
+            int old = a.Items.Count;
 
-        //    VereineViewModel b = new VereineViewModel();
-        //    b.Items.Add(verein);
+            a.Items.Add(verein);
 
-        //    int neu = a.Items.Count;
+            int neu = a.Items.Count;
 
-        //    Assert.IsTrue(neu == old + 1);
-        //}
+            Assert.IsTrue(neu == old + 1);
+        }
 
         [Test]
-        public async Task TestLogin()
+        public async Task TestRemovePerson()
         {
-            var container = ContainerExtensions
-                .CreateContainer()
-                .RegisterWettkampfServices();
-
-
             var dialogService = A.Fake<IDialogService>();
+            App.Services.RegisterInstance(dialogService);
 
+            var factory = A.Fake<IItemFactory<Verein>>();
+            App.Services.RegisterInstance(factory);
+
+            Verein verein = new Verein();
+
+            ListViewModelBase<Verein, VereineViewModel> a = new ListViewModelBase<Verein, VereineViewModel>();
+
+            a.Items.Add(verein);
+            a.Items.Add(verein);
+
+            int old = a.Items.Count;
+
+            a.Items.Remove(verein);
+            int neu = a.Items.Count;
+
+            Assert.IsTrue(neu == old - 1);
+        }
+
+        [Test]
+        public async Task TestPasswordAdminOk()
+        {
+            var dialogService = A.Fake<IDialogService>();
             App.Services.RegisterInstance(dialogService);
 
             LoginViewModel a = new LoginViewModel();
-            //bool c = await a.checkLogin("Admin", "admin");
 
-
-            Assert.That(await a.CheckLogin("Admin", "admin"), Is.True);
-            A.CallTo(() => dialogService.Show(A<string>.Ignored, A<string>.Ignored)).MustHaveHappenedOnceExactly();
+            Assert.That(await a.CheckLogin("GrZKy/5QWp7WrKf21hNZUA==", "Admin"), Is.True);
         }
 
         [Test]
-        public async Task Testtest()
+        public async Task TestPasswordAdminFalse()
         {
-            var container = ContainerExtensions
-                .CreateContainer()
-                .RegisterWettkampfServices();
-
             var dialogService = A.Fake<IDialogService>();
+            App.Services.RegisterInstance(dialogService);
 
-            container.RegisterInstance(dialogService);
+            LoginViewModel a = new LoginViewModel();
 
-            var vereinSaver = container.GetInstance<IVereinSaver>();
-
-            var verein = new Verein();
-
-            Assert.That(await vereinSaver.TrySaveAsync(verein), Is.False);
-            A.CallTo(() => dialogService.Show(A<string>.Ignored, A<string>.Ignored)).MustHaveHappenedOnceExactly();
+            Assert.That(await a.CheckLogin("1234", "Admin"), Is.False);
         }
 
+        [Test]
+        public async Task TestPasswordUserOk()
+        {
+            var dialogService = A.Fake<IDialogService>();
+            App.Services.RegisterInstance(dialogService);
 
-    }
+            LoginViewModel a = new LoginViewModel();
+
+            Assert.That(await a.CheckLogin("IxA5mJA4ZpFBeiAsWLTFRQ==", "User"), Is.True);
+        }
+
+        [Test]
+        public async Task TestPasswordUserFalse()
+        {
+            var dialogService = A.Fake<IDialogService>();
+            App.Services.RegisterInstance(dialogService);
+
+            LoginViewModel a = new LoginViewModel();
+
+            Assert.That(await a.CheckLogin("1234", "User"), Is.False);
+        }
+  }
 }
